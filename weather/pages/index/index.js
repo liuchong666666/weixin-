78ea8eda0a7005f914c41e81cacbaaf58532cf91
刘冲 <liuchong@bdfint.com>
2019-10-31 18:25:00 +0800

    selfloaction

--- weather/pages/index/index.js
+++ weather/pages/index/index.js
@@ -1,4 +1,4 @@
-var QQMapWX = require('../../qqmap-wx-jssdk1.2/qqmap-wx-jssdk.js')// 引入SDK核心类
+var QQMapWX = require('../../qqmap-wx-jssdk1.2/qqmap-wx-jssdk.js') // 引入SDK核心类
 var qqmapsdk;
 Page({
   /**
@@ -7,37 +7,37 @@ Page({
   data: {
     // 省 市 区，用市来调天气
     region: ['北京市', '北京市', '东城区'],
-    now:''
+    now: ''
   },
   changeRegion: function(e) {
     this.setData({
       region: e.detail.value
     })
-    this.getWeather();//更新天气
+    this.getWeather(); //更新天气
   },
   // 实例化地址API的核心类
   //参考https://lbs.qq.com/qqmap_wx_jssdk/method-getcitylist.html
   //询问获取用户地址权限：https://blog.csdn.net/shanshan_1117/article/details/88994160
-  getAPI:function(){
+  getAPI: function() {
     qqmapsdk = new QQMapWX({
-      key:'7VJBZ-X26KU-WE6VH-BPU45-ZCWMH-WNBCD'
+      key: '7VJBZ-X26KU-WE6VH-BPU45-ZCWMH-WNBCD'
     })
   },
   // 调用获取地址接口
-  getLocation:function(){
+  getLocation: function(callback) {
     var that = this;
     qqmapsdk.search({
       keyword: '酒店',
-      success: function (res) {
+      success: function(res) {
         console.log(res.data[0].ad_info)
         that.setData({
           region: [res.data[0].ad_info.province, res.data[0].ad_info.city, res.data[0].ad_info.district]
-        })
+        }, () => that.getWeather())//获取地址后，再获取天气，异步操作用回调
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res);
       },
-      complete: function (res) {
+      complete: function(res) {
         // console.log(res);
       }
     })
@@ -55,16 +55,22 @@ Page({
       success: function(res) {
         // console.log(res.data)
         that.setData({
-          now:res.data.HeWeather6[0].now
+          now: res.data.HeWeather6[0].now
         })
       }
     })
+  },
+  //获取自己的位置
+  getSelfLocation: function() {
+    this.getLocation();
+    // this.getWeather();
+    
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.getWeather();
+    // this.getWeather();
     this.getAPI();
   },
 
